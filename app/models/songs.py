bac7b39783409c1cd9db104e11ed1d6bf7096b03
Dylan <dylanbrookschi@gmail.com>
2021-05-26 18:34:44 -0500

    i think i actually fixed the model

--- app/models/songs.py
+++ app/models/songs.py
@@ -7,7 +7,7 @@ class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     albumArtUrl = db.Column(db.String(250), nullable=False)
-    lyrics = db.Column(db.String, nullable=False)
+    lyrics = db.Column(db.String(5000), nullable=False)
     artistId = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
