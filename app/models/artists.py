6ab8a0f78cc0ef4c79173d225b89225d1f161c3b
Dylan <dylanbrookschi@gmail.com>
2021-05-26 18:18:01 -0500

    seeder should actually be fixed lol

--- app/models/artists.py
+++ app/models/artists.py
@@ -5,8 +5,8 @@ class Artist(db.Model):
     __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    imageUrl = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    imageUrl = db.Column(db.String(250), nullable=False)
 
     def to_dict(self):
         return {
