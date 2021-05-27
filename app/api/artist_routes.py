10cf3dcbe7506f7e2b252e12f73d369c87d2c469
Dylan <dylanbrookschi@gmail.com>
2021-05-27 12:24:32 -0500

    commented back in

--- app/api/artist_routes.py
+++ app/api/artist_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, Artist
+from app.models import db, Artist, Song
 
 artist_routes = Blueprint("artists", __name__)
-# song_routes = Blueprint("songs", __name__)
+song_routes = Blueprint("songs", __name__)
 translation_routes = Blueprint("translations", __name__)
 
 @artist_routes.route("/")
@@ -10,26 +10,26 @@ def artists():
     artists = Artist.query.all()
     return {"artists" : [artist.to_dict() for artist in artists]}
 
-# @artist_routes.route("/<int:id>")
-# def songs(id):
-#     artist = Artist.query.get(id)
-#     songs = Song.query.filter(Song.artistId == id).limit(10)
-#     return {
-#         "artist": artist.to_dict(),
-#         "songs": [song.to_dict() for song in songs]
-#     }
+@artist_routes.route("/<int:id>")
+def songs(id):
+    artist = Artist.query.get(id)
+    songs = Song.query.filter(Song.artistId == id).limit(10)
+    return {
+        "artist": artist.to_dict(),
+        "songs": [song.to_dict() for song in songs]
+    }
 
 
-# @song_routes.route("/<int:id>")
-# def lyrics(id):
-#     song = Song.query.get(id)
-#     return {
-#         "song": song.to_dict()
-#     }
+@song_routes.route("/<int:id>")
+def lyrics(id):
+    song = Song.query.get(id)
+    return {
+        "song": song.to_dict()
+    }
 
-# @translation_routes.route("/<int:id>")
-# def translations(id):
-#     translation = Translation.query.get(id)
-#     return {
-#         "translation": translation.to_dict()
-#     }
+@translation_routes.route("/<int:id>")
+def translations(id):
+    translation = Translation.query.get(id)
+    return {
+        "translation": translation.to_dict()
+    }
