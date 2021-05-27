67271748b2be6f71c1fb9d5200ad3d744cc844cd
Dylan <dylanbrookschi@gmail.com>
2021-05-27 03:27:43 -0500

    another test

--- app/models/songs.py
+++ app/models/songs.py
@@ -5,9 +5,9 @@ class Song(db.Model):
     __tablename__ = 'songs'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    albumArtUrl = db.Column(db.String(250), nullable=False)
-    lyrics = db.Column(db.String(5000), nullable=False)
+    name = db.Column(db.String, nullable=False)
+    albumArtUrl = db.Column(db.String, nullable=False)
+    lyrics = db.Column(db.String, nullable=False)
     artistId = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
